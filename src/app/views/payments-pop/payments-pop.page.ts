f4d590d583be9503649b8bf509552b61e58a530a
Tendai Karuma <55027177+protendai@users.noreply.github.com>
2023-03-23 00:07:18 +0200

    added payment pop

--- src/app/views/payments-pop/payments-pop.page.ts
+++ src/app/views/payments-pop/payments-pop.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { DatabaseService } from 'src/app/services/database.service';
+import { NotificationsService } from 'src/app/services/notifications.service';
+import { SqliteService } from 'src/app/services/sqlite.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-payments-pop',
@@ -8,13 +13,55 @@ import { Router } from '@angular/router';
 })
 export class PaymentsPopPage implements OnInit {
 
-  constructor(private router:Router) { }
+  public data = {
+    type:'',
+    names:'',
+    phone:'',
+    pop:'',
+  };
+
+  constructor(
+    private router:Router,
+    private apiService: ApiService,
+    private storageService: StorageService,
+    private notificationService: NotificationsService,
+    private sqliteService : SqliteService,
+    private databaseService: DatabaseService
+    ) { }
 
   ngOnInit() {
   }
 
   goBack(){
     this.router.navigate(['/tabs']);
+  }
+
+  upload(){
+ 
+     console.log(this.data);
+
+      // if(this.data.names === "" || this.data.pop === "" || this.data.phone === ""){
+      //   this.notificationService.presentAlert("Registration failed","Please fill in all required fields");
+      // }else{
+      //   console.log(this.data);
+       
+      //   this.notificationService.showLoader('Registering ...');
+      //   this.apiService.register(this.data).subscribe(async (v)=>{
+      //     this.notificationService.dismissLoader();
+      //     console.log(v);
+   
+      //       if(v.success){
+      //         console.log(v.success);
+      //       }
+          
+      //   },(error) => {
+      //     console.log(error.error);
+      //     this.notificationService.dismissLoader();
+      //     this.notificationService.presentAlert("Registration failed","Please try again");
+      //   });
+    
+      // }
+
   }
 
 
